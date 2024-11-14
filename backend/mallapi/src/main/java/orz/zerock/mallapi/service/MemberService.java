0ff2781264eb4533131dfac02058d6eabf7eb931
DESKTOP-SRV7RUU\home <snf00020@naver.com>
2024-11-14 20:53:52 +0900

    413p

---
+++ backend/mallapi/src/main/java/orz/zerock/mallapi/service/MemberService.java
@@ -0,0 +1,25 @@
+package orz.zerock.mallapi.service;
+
+import org.springframework.transaction.annotation.Transactional;
+import orz.zerock.mallapi.domain.Member;
+import orz.zerock.mallapi.dto.MemberDTO;
+
+import java.util.stream.Collectors;
+
+@Transactional
+public interface MemberService {
+
+    MemberDTO getKakaoMember(String accessToken);
+
+    default MemberDTO entityToDTO(Member member) {
+
+        MemberDTO dto = new MemberDTO(
+                member.getEmail(),
+                member.getPw(),
+                member.getNickname(),
+                member.isSocial(),
+                member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList())
+        );
+        return dto;
+    }
+}
